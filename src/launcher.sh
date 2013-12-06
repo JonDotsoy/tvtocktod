6edc9cee6d335ae95cb3cbb48670c930f354e55a
Jonathan <jonathan@jonathan-pc.(none)>
2013-12-06 02:33:25 -0300

    Upload 1.02

---
+++ src/launcher.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Bosoleto!!
+#
+
+# echo $*;
+
+
+c1="ETC TV"
+c2="13C"
+c4="La Red"
+c5="UCV"
+c9="Mega"
+c11="CHV"
+c13="Canal 13"
+
+canal=""
+
+for ln in $*
+do
+	case $ln in
+		*)
+			canal="$canal $ln"
+		;;
+	esac
+done
+
+case $* in
+	1) canal=$c1 ;;
+	2) canal=$c2 ;;
+	4) canal=$c4 ;;
+	5) canal=$c5 ;;
+	9) canal=$c9 ;;
+	11) canal=$c11 ;;
+	13) canal=$c13 ;;
+esac
+
+google-chrome --app="chrome-extension://ijdncbaekoleeeddhkbnoihmmlfalmkb/canal/ver.html#$canal"
+
