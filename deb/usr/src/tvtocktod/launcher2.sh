e5012d214ba33197be90dde4d3176c4f69d4d43c
root <root@jonathan-pc.(none)>
2013-12-06 20:00:28 -0300

    Upload 1.06

---
+++ deb/usr/src/tvtocktod/launcher2.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Bosoleto!!
+#
+
+# echo $*;
+
+
+c1="ETC TV"
+c2="13C"
+c4="La Red"
+c5="UCV"
+c9="Mega"
+c11="CHV"
+c13="Canal 13"
+
+canal=""
+
+for ln in $*
+do
+	case $ln in
+		*)
+			canal="$canal $ln"
+		;;
+	esac
+done
+
+case $* in
+	1) canal=$c1 ;;
+	2) canal=$c2 ;;
+	4) canal=$c4 ;;
+	5) canal=$c5 ;;
+	9) canal=$c9 ;;
+	11) canal=$c11 ;;
+	13) canal=$c13 ;;
+esac
+
+if [ !-z $canal ]
+	then
+	google-chrome --app=chrome-extension://phimhnckkaofkllcoledjilakgbeohli/popup.html
+	exit
+fi
+
+google-chrome --app="chrome-extension://phimhnckkaofkllcoledjilakgbeohli/canal/ver.html#$canal"
+
